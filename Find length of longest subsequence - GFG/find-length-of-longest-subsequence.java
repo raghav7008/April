669f7834978c7c12e4097387439873661f0e09dd
raghav7008 <83825556+raghav7008@users.noreply.github.com>
2022-04-03 22:43:02 +0530

    Added solution - LeetHub

---
+++ Find length of longest subsequence - GFG/find-length-of-longest-subsequence.java
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(input_line[0]);
+            int M = Integer.parseInt(input_line[1]);
+            input_line = read.readLine().trim().split("\\s+");
+            String X = input_line[0];
+            String Y = input_line[1];
+            Solution obj = new Solution();
+            int ans = obj.maxSubsequenceSubstring(X, Y, N, M); 
+            System.out.println(ans);
+        }
+    } 
+} // } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    int maxSubsequenceSubstring(String X, String Y, int N, int M){
+        // int[][] dp = new int[n+1][m+1];
+        // for (int i = 0; i <= n; i++)
+        //     for (int j = 0; j <= m; j++)
+        //         dp[i][j] = 0;
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= m; j++){
+        //         if(x.charAt(i-1) == y.charAt(j-1)){
+        //             dp[i][j] = dp[i-1][j-1] + 1;
+        //         }
+        //         else    dp[i][j] = dp[i-1][j];
+        //     }
+        // }
+        // return dp[n][m];
+        int max=0;
+        for(int i=0;i<Y.length();i++) {
+            int k=i;
+            int req=0;
+            for(int j=0;j<X.length() && k<Y.length();j++) {
+                if(X.charAt(j)==Y.charAt(k)) {
+                    k++;
+                    req++;
+                }
+            }
+            max=Math.max(max, req);
+        }
+        return max;
+    } 
+} 
