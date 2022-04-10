1c219f37aa6cc6d9d0c25aea3c7f5c5f70a77860
raghav7008 <83825556+raghav7008@users.noreply.github.com>
2022-04-10 12:39:34 +0530

    Time: 1 ms (96.26%), Space: 41.2 MB (99.57%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -1,15 +1,22 @@
 class Solution {
     public int minOperations(String[] logs) {
-        List<String> list = new ArrayList();
-        for(int i = 0; i < logs.length ; i++){
-            if(logs[i].equals("./"))    continue;
-            else if(logs[i].equals("../")){
-                if(list.size() > 0) list.remove(list.size()-1);
-            }
-            else{
-                list.add("V");
-            }
+        // List<String> list = new ArrayList();
+        // for(int i = 0; i < logs.length ; i++){
+        //     if(logs[i].equals("./"))    continue;
+        //     else if(logs[i].equals("../")){
+        //         if(list.size() > 0) list.remove(list.size()-1);
+        //     }
+        //     else{
+        //         list.add("V");
+        //     }
+        // }
+        // return list.size();
+        int res = 0;
+        for (String s : logs) {
+            if (s.equals("../")) res = Math.max(0, --res);
+            else if (s.equals("./")) continue;
+            else res++;
         }
-        return list.size();
+        return res;
     }
 }
