4cc246d2e1d6af9e545d5d6c056ab558d8c7fa67
raghav7008 <83825556+raghav7008@users.noreply.github.com>
2022-04-10 16:52:38 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (91.48%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,34 +1,43 @@
 class Solution {
-    public String mergeAlternately(String word1, String word2) {
-        int i = 0, j = 0;
-        boolean flag = true;
-        String res = "";
-        while(i < word1.length() && j < word2.length()){
-            if(flag){
-                res += word1.charAt(i);
-                i++;
-                flag = false;
-            }
-            else{
-                res += word2.charAt(j);
-                j++;
-                flag = true;
-            }
-        }
-        if(i != word1.length()){
-            while(i < word1.length()){
-                res += word1.charAt(i);
-                i++;
-            }
-            return res;
-        }
-        if(j != word2.length()){
-            while(j < word2.length()){
-                res += word2.charAt(j);
-                j++;
-            }
+    public String mergeAlternately(String w1, String w2) {
+//         int i = 0, j = 0;
+//         boolean flag = true;
+//         String res = "";
+//         while(i < word1.length() && j < word2.length()){
+//             if(flag){
+//                 res += word1.charAt(i);
+//                 i++;
+//                 flag = false;
+//             }
+//             else{
+//                 res += word2.charAt(j);
+//                 j++;
+//                 flag = true;
+//             }
+//         }
+//         if(i != word1.length()){
+//             while(i < word1.length()){
+//                 res += word1.charAt(i);
+//                 i++;
+//             }
+//             return res;
+//         }
+//         if(j != word2.length()){
+//             while(j < word2.length()){
+//                 res += word2.charAt(j);
+//                 j++;
+//             }
             
+//         }
+//         return res;
+        int n = w1.length(), m = w2.length(), i = 0, j = 0;
+        StringBuilder res = new StringBuilder();
+        while (i < n || j < m) {
+            if (i < w1.length())
+                res.append(w1.charAt(i++));
+            if (j < w2.length())
+                res.append(w2.charAt(j++));
         }
-        return res;
+        return res.toString();
     }
 }
