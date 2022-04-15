1837ff8a0aaa36735e5c8af3cf59955ec97d32c5
raghav7008 <83825556+raghav7008@users.noreply.github.com>
2022-04-15 23:37:23 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -30,26 +30,55 @@ class GFG {
     }
 }// } Driver Code Ends
 
-
+class Node{
+    int first;
+    int second;
+    public Node(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+}
 class Solution {
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int v, ArrayList<ArrayList<Integer>> adj) {
+        // boolean[] vis = new boolean[v];
+        // for(int i = 0; i < v; i++){
+        //     if(!vis[i]){
+        //         if(dfs(i, -1, vis, adj))    return true;
+        //     }
+        // }
+        // return false;
         boolean[] vis = new boolean[v];
         for(int i = 0; i < v; i++){
             if(!vis[i]){
-                if(dfs(i, -1, vis, adj))    return true;
+                Queue<Node> q = new LinkedList();
+                q.add(new Node(i,-1));
+                vis[i] = true;
+                while(!q.isEmpty()){
+                    int curr = q.peek().first;
+                    int par = q.peek().second;
+                    vis[curr] = true;
+                    q.remove();
+                    for(int it: adj.get(curr)){
+                        if(!vis[it]){
+                            q.add(new Node(it,curr));
+                        }
+                        else if(par != it)  return true;
+                    }
+                }
             }
         }
         return false;
+        
     }
-    public boolean dfs(int curr, int par, boolean[] vis, ArrayList<ArrayList<Integer>> adj){
-        vis[curr] = true;
-        for(int it: adj.get(curr)){
-            if(!vis[it]){
-                if(dfs(it, curr, vis, adj)) return true;
-            }
-            else if(par != it)  return true;
-        }
-        return false;
-    }
+    // public boolean dfs(int curr, int par, boolean[] vis, ArrayList<ArrayList<Integer>> adj){
+    //     vis[curr] = true;
+    //     for(int it: adj.get(curr)){
+    //         if(!vis[it]){
+    //             if(dfs(it, curr, vis, adj)) return true;
+    //         }
+    //         else if(par != it)  return true;
+    //     }
+    //     return false;
+    // }
 }
