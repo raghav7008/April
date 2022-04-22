17d148cba2d6a30eefec0d47248f02779397e12b
raghav7008 <83825556+raghav7008@users.noreply.github.com>
2022-04-22 13:40:10 +0530

    Time: 2 ms (99.87%), Space: 50.4 MB (76.74%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -1,16 +1,25 @@
 class Solution {
-    public int findJudge(int n, int[][] trust) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i = 1; i <= n; i++) set.add(i);
-        for(int i = 0; i < trust.length; i++)   set.remove(trust[i][0]);
-        if(set.size() == 0)  return -1;
-        int num = 0;
-        for(int x : set)    num = x;
-        int count = 0;
-        for(int i = 0; i < trust.length; i++){
-            if(trust[i][1] == num)  count++;
+    public int findJudge(int N, int[][] trust) {
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int i = 1; i <= n; i++) set.add(i);
+        // for(int i = 0; i < trust.length; i++)   set.remove(trust[i][0]);
+        // if(set.size() == 0)  return -1;
+        // int num = 0;
+        // for(int x : set)    num = x;
+        // int count = 0;
+        // for(int i = 0; i < trust.length; i++){
+        //     if(trust[i][1] == num)  count++;
+        // }
+        // if(count == n-1)  return num;
+        // return -1;
+        int[] count = new int[N+1];
+        for (int[] t: trust) {
+            count[t[0]]--;
+            count[t[1]]++;
         }
-        if(count == n-1)  return num;
+        for (int i = 1; i <= N; ++i) {
+            if (count[i] == N - 1) return i;
+        }
         return -1;
     }
 }
