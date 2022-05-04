31d7709259a4c89b8c31594afdfacc7a319ba3a7
raghav7008 <83825556+raghav7008@users.noreply.github.com>
2022-05-04 20:07:27 +0530

    Time: 60 ms (28.82%), Space: 84.3 MB (26.53%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -3,18 +3,15 @@ class Solution {
         int count = 0;
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
-            if(map.containsKey(k - nums[i]) && map.get(k - nums[i]) > 0 && nums[i] != k - nums[i]){
+            if(map.containsKey(k - nums[i]) && map.get(k - nums[i]) > 0){
                 count++;
                 map.put(k-nums[i], map.get(k-nums[i])-1);
-                map.put(nums[i], map.get(nums[i])-1);
             }
-            else if(map.containsKey(k-nums[i]) && nums[i] == k - nums[i] && map.get(nums[i]) > 1){
-                count++;
-                map.put(nums[i], map.get(nums[i]) - 2);
+            else {
+                map.put(nums[i], map.getOrDefault(nums[i],0)+1);
             }
         }
-        System.out.println(map);
+        //System.out.println(map);
         return count;
     }
 }
