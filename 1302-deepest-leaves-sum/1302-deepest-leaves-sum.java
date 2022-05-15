638493b9ba3357b1ecadc2a7e9d876b3c0044a0c
raghav7008 <83825556+raghav7008@users.noreply.github.com>
2022-05-15 17:53:31 +0530

    Time: 12 ms (19.19%), Space: 59.5 MB (24.98%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -16,39 +16,44 @@
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
         if(root == null)    return 0;
-        Queue<Pair<TreeNode,Integer>> q = new LinkedList<Pair<TreeNode,Integer>>();
-        ArrayList<Pair<TreeNode,Integer>> list = new ArrayList();
-        q.offer(new Pair(root, 1));
+        // Queue<Pair<TreeNode,Integer>> q = new LinkedList<Pair<TreeNode,Integer>>();
+        // ArrayList<Pair<TreeNode,Integer>> list = new ArrayList();
+        // q.offer(new Pair(root, 1));
+        // while(!q.isEmpty()){
+        //     int size = q.size();
+        //     for(int i = 0; i < size; i++){
+        //         Pair temp = q.remove();
+        //         TreeNode node = (TreeNode)temp.getKey();
+        //         int level = (int)temp.getValue();
+        //         list.add(new Pair(node, level));
+        //         if(node.left != null)   q.offer(new Pair(node.left, level + 1));
+        //         if(node.right != null)   q.offer(new Pair(node.right, level + 1));
+        //         //System.out.println(node.val + " " + level);
+        //     }
+        // }
+        // int max = Integer.MIN_VALUE;
+        // for(int i = 0; i < list.size(); i++){
+        //     max = Math.max(max, list.get(i).getValue());
+        // }
+        // int sum = 0;
+        // for(int i = 0; i < list.size(); i++){
+        //     if(list.get(i).getValue() == max){
+        //         sum += list.get(i).getKey().val;
+        //     }
+        // }
+        // return sum;
+        Queue<TreeNode> q = new LinkedList<>();
+        int i, res = 0;
+        q.offer(root);
         while(!q.isEmpty()){
             int size = q.size();
-            for(int i = 0; i < size; i++){
-                Pair temp = q.remove();
-                TreeNode node = (TreeNode)temp.getKey();
-                int level = (int)temp.getValue();
-                list.add(new Pair(node, level));
-                if(node.left != null)   q.offer(new Pair(node.left, level + 1));
-                if(node.right != null)   q.offer(new Pair(node.right, level + 1));
-                //System.out.println(node.val + " " + level);
+            for( i = 0, res = 0; i < size; i++){
+                TreeNode temp = q.remove();
+                res += temp.val;
+                if(temp.left != null)   q.offer(temp.left);
+                if(temp.right != null)   q.offer(temp.right);
             }
         }
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < list.size(); i++){
-            max = Math.max(max, list.get(i).getValue());
-        }
-        int sum = 0;
-        for(int i = 0; i < list.size(); i++){
-            if(list.get(i).getValue() == max){
-                sum += list.get(i).getKey().val;
-            }
-        }
-        return sum;
+        return res;
     }
 }
-// class Pair{
-//     TreeNode node;
-//     int level;
-//     Pair(TreeNode node, int level){
-//         this.node = node;
-//         this.level = level;
-//     }
-// }
