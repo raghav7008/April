24cb657609ab2d365416d540443bf8a88a7113a6
raghav7008 <83825556+raghav7008@users.noreply.github.com>
2022-06-08 13:06:13 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (47.22%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int removePalindromeSub(String s) {
+        if(s.length() == 0) return 0;
+        if(s.equals(new StringBuilder(s).reverse().toString())) return 1;
+        return 2;
+    }
+}
